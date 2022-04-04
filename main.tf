af01fd2087be1181e01929b988b9b779af5e9493
Joyc132 <95086676+Joyc132@users.noreply.github.com>
2022-04-04 12:58:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,46 @@
-module "ec2" {
-  source = "./modules/ec2"
-
-  region = "${var.region}"
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
 }
+
+provider "google" {
+  version = "3.5.0"
+
+  credentials = file(var.credentials_file)
+
+  project = var.project
+  region  = var.region
+  zone    = var.zone
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = var.instance_name
+  machine_type = var.machine_type
+  tags         = var.network_tags
+
+  boot_disk {
+    initialize_params {
+      image = var.machine_image
+      type  = var.disk_type
+      size  = var.disk_size
+    }
+  }
+
+  scheduling {
+    preemptible       = true
+    automatic_restart = false
+  }
+
+  metadata_startup_script = file(var.startup_script)
+
+
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+}
+
