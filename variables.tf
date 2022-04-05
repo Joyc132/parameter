d949c3dcb3734ce11d8a3dcdede2d486fd6c870e
Joyc132 <95086676+Joyc132@users.noreply.github.com>
2022-04-05 10:54:59 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "region" {}
+variable "gcp_project" {}
+variable "credentials" {}
+variable "name" {}
+variable "subnet_cidr" {}
+variable "instance_name" {}
+variable "machine_type" {}
+variable "image" {}
